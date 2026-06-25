8f3077c5dbd51389f0c4805a531101d56a0acef6
HyperProgramming <jrdcprogrammings@gmail.com>
2026-06-26 00:47:44 +0800

    Updated game logic with new features
    added def vs def logic
    refined heal vs atk logic
    added menu
    added start game and mechanics description
    added a pause between rounds
    improved readability

--- Python/turnBasedGame.py
+++ Python/turnBasedGame.py
@@ -30,6 +30,40 @@ import random
 # after game ends display:
   # total rounds played; final hp of both players; winner;
 
+
+while True:
+    print("1 on 1 turn based game")
+    print("[1] Start\n[2] Mechanics")
+    c = int(input("Choice: "))
+    # introduction
+    if c == 2:
+        print("Welcome to my turn based 1 on 1 game!")
+        print("The game has a max of 20 rounds and each round you draw 3 random cards")
+        print("\tATK, DEF, and HEAL")
+        print("Each card receives a random value")
+        print("\t1, 100, 200, 300, 400, 500, and 1000")
+        print("A player gains a skill point (SP) depending on the card's value")
+        print("\t1 = 0, 100 = 1, 200 = 2, 300 = 3, 400 = 4, 500 = 5, 1000 = 10")
+        print("The max SP a player can hold is 20\n")
+        print("Card phases")
+        print("\tatk vs atk = both deal damage to each other")
+        print("\tatk vs def = compare atk value to def value")
+        print("\t\tif atk > def: subtract defender hp")
+        print("\t\tif atk < def: subtract attacker's hp")
+        print("\t\tif atk == def: no damage")
+        print("\tdef vs def = nothing happens")
+        print("\theal vs heal = both heal")
+        print("\theal vs atk = heal has priority over atk")
+        print("\theal vs def = player heals while def is useless\n")
+        print("special skills:")
+        print("\tunique skill (10 sp) = deals random dmg between 1k-3k, prompts everytime if player has 10 sp")
+        print("\tult skill (20 sp) = deals random dmg between 3k-5k, prompts everytime if player has 20 sp\n")
+    elif c == 1:
+        print("")
+        break
+    else:
+        print("Invalid Input")
+
 # ask each p name
 p1N = input("Enter Player 1 Name: ")   
 p2N = input("Enter Player 2 Name: ")   
@@ -46,7 +80,10 @@ cards = ["atk", "def", "heal"]
 cVal = [1, 100, 200, 300, 400, 500, 1000]
 
 for round in range(1, 21):
-    print("\n=== ROUND", round, "===")
+    print("")
+    print("=" * 40)
+    print("\t\tROUND", round)
+    print("=" * 40)
     # special skills
     if p1sp == 20:
         while True:
@@ -137,8 +174,8 @@ for round in range(1, 21):
         if p2sp > 20:
             p2sp = 20
     
-    print(p1N, "drew", p1C, "card, with value", p1V, "\nSP gained:", p1spG, ",", p1N, "has a total of", p1sp, "skill points!\n")
-    print(p2N, "drew", p2C, "card, with value", p2V, "\nSP gained:", p2spG, ",", p2N, "has a total of", p2sp, "skill points!\n")
+    print(p1N, "drew", p1C, "card, with value", p1V, "\n  SP gained:", p1spG, ",", p1N, "has a total of", p1sp, "skill points!\n")
+    print(p2N, "drew", p2C, "card, with value", p2V, "\n  SP gained:", p2spG, ",", p2N, "has a total of", p2sp, "skill points!\n")
 
     # card phase
     if p1C == "atk" and p2C == "atk":
@@ -167,6 +204,8 @@ for round in range(1, 21):
             print(p2N, "has higher stat!", p1N, "negated", p1V, "took", dmg, "damage\n")
         elif p1V == p2V:
             print("Both values are equal, no damage taken for both sides\n")
+    elif p1C == "def" and p2C == "def":
+        print("Both took defensive stances, nothing happens\n")
     elif p1C == "heal" and p2C == "heal":
         p1hp += p1V
         p2hp += p2V
@@ -188,21 +227,22 @@ for round in range(1, 21):
     elif p1C == "heal" and p2C == "def":
         p1hp += p1V
         print(p1N, "healed", p1V, "hp")
-        print(p2N, "defends\nNothing happens..")
+        print(p2N, "defends\n\nNothing happens..\n")
     elif p1C == "def" and p2C == "heal":
         p2hp += p2V
         print(p2N, "healed", p2V, "hp")
-        print(p1N, "defends\nNothing happens..")
+        print(p1N, "defends\n\nNothing happens..\n") 
     
     print("--- Remaining HP ---")
     print(p1N, ":", p1hp, "|", p2N, ":", p2hp)
 
     if p1hp <= 0 or p2hp <= 0:
         break
+    input("Press enter to continue..")
 
 # game over screen
 print("\n=== GAME OVER ===")
-print("Played a total of", round, "rounds")
+print("Played a total of", round, "rounds\n")
 print("--- Remaining HP ---")
 print(p1N, ":", p1hp)
 print(p2N, ":", p2hp)
@@ -212,11 +252,12 @@ if p1hp <= 0 and p2hp <= 0:
 elif p1hp <= 0:
     print("The winner is", p2N)
 elif p2hp <= 0:
-    print("The winner is", p1N)
+    print("The winner is", p1N) 
 else:
     if p1hp > p2hp:
         print("20 rounds has passed, the winner is", p1N)
     elif p2hp > p1hp:
         print("20 rounds has passed, the winner is", p2N)
     else:
-        print("20 rounds has passed, both have equal standing.. It's a draw!")  
+        print("20 rounds has passed, both have equal standing.. It's a draw!")
+input("\nGame over, press enter to exit..")
